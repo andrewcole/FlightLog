6046c76ec7049b28ae7e301621a1a4b7a7de733c
Andrew Cole <andrew.cole@illallangi.com>
2014-01-04 23:28:22 +1100

    Added Zoned times to flights

--- src/Illallangi.FlightLog/Model/Flight.cs
+++ src/Illallangi.FlightLog/Model/Flight.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using NodaTime;
 
 namespace Illallangi.FlightLog.Model
 {
     public class Flight
     {
+        #region Fields
+
+        private ZonedDateTime? currentDepartureZoned;
+
+        private ZonedDateTime? currentArrivalZoned;
+
+        #endregion
+
         #region Properties
 
         #region Primary Key Property
@@ -51,6 +60,32 @@ namespace Illallangi.FlightLog.Model
         #endregion
 
         #region Calculated Properties
+
+        public ZonedDateTime DepartureZoned
+        {
+            get
+            {
+                return (this.currentDepartureZoned.HasValue ?
+                            this.currentDepartureZoned :
+                            this.currentDepartureZoned =
+                                DateTimeZoneProviders.Tzdb[this.OriginTimezone].ResolveLocal(
+                                    LocalDateTime.FromDateTime(this.Departure),
+                                NodaTime.TimeZones.Resolvers.LenientResolver)).Value;
+            }
+        }
+
+        public ZonedDateTime ArrivalZoned
+        {
+            get
+            {
+                return (this.currentArrivalZoned.HasValue ?
+                            this.currentArrivalZoned :
+                            this.currentArrivalZoned =
+                                DateTimeZoneProviders.Tzdb[this.DestinationTimezone].ResolveLocal(
+                                    LocalDateTime.FromDateTime(this.Arrival),
+                                    NodaTime.TimeZones.Resolvers.LenientResolver)).Value;
+            }
+        }
 
         #endregion
 
