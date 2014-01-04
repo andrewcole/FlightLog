d8c1e9bd480151e2b2b9afe9cecaecb63b4b493b
Andrew Cole <andrew.cole@illallangi.com>
2014-01-04 19:16:27 +1100

    Removed unneeded filter parameters

--- src/Illallangi.FlightLog/PowerShell/GetAirportCmdlet.cs
+++ src/Illallangi.FlightLog/PowerShell/GetAirportCmdlet.cs
@@ -20,21 +20,6 @@ namespace Illallangi.FlightLog.PowerShell
         public string CountryName { get; set; }
 
         [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public string Dst { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Timezone { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Altitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Longitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Latitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
         public string Icao { get; set; }
 
         [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
@@ -43,9 +28,7 @@ namespace Illallangi.FlightLog.PowerShell
         protected override void BeginProcessing()
         {
             this.WriteObject(this.Repository.Retrieve(new Airport { Id = this.Id, Name = this.Name, 
-                CityName = this.CityName, CountryName = this.CountryName, Iata = this.Iata, Icao = this.Icao, 
-                Latitude = this.Latitude, Longitude = this.Longitude, Altitude = this.Altitude, 
-                Timezone = this.Timezone, Dst = this.Dst } ), true);
+                City = this.CityName, Country = this.CountryName, Iata = this.Iata, Icao = this.Icao } ), true);
         }
 
     }
--- src/Illallangi.FlightLog/PowerShell/RemoveAirportCmdlet.cs
+++ src/Illallangi.FlightLog/PowerShell/RemoveAirportCmdlet.cs
@@ -21,21 +21,6 @@ namespace Illallangi.FlightLog.PowerShell
         public string CountryName { get; set; }
 
         [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public string Dst { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Timezone { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Altitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Longitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
-        public float? Latitude { get; set; }
-
-        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
         public string Icao { get; set; }
 
         [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = "Value")]
@@ -44,9 +29,7 @@ namespace Illallangi.FlightLog.PowerShell
         protected override void BeginProcessing()
         {
             foreach (var o in this.Repository.Retrieve(new Airport { Id = this.Id, Name = this.Name, 
-                CityName = this.CityName, CountryName = this.CountryName, Iata = this.Iata, Icao = this.Icao, 
-                Latitude = this.Latitude, Longitude = this.Longitude, Altitude = this.Altitude, 
-                Timezone = this.Timezone, Dst = this.Dst } ).ToList().Where(o => this.ShouldProcess(o.ToString(), VerbsCommon.Remove)))
+                City = this.CityName, Country = this.CountryName, Iata = this.Iata, Icao = this.Icao } ).ToList().Where(o => this.ShouldProcess(o.ToString(), VerbsCommon.Remove)))
             {
                 this.Repository.Delete(o);
             }
