8d25867b41a96a2346cc37fcf4b495a7651a71d8
Andrew Cole <andrew.cole@illallangi.com>
2014-01-24 20:52:06 +1100

    Updated LiteOrm to 1.0.14

--- src/Illallangi.FlightLog.PowerShell/Country/GetCountryCmdlet.cs
+++ src/Illallangi.FlightLog.PowerShell/Country/GetCountryCmdlet.cs
@@ -1,15 +1,44 @@
 ﻿namespace Illallangi.FlightLog.PowerShell.Country
 {
+    using System;
     using System.Management.Automation;
 
     using Illallangi.FlightLog.Model;
 
     [Cmdlet(VerbsCommon.Get, Nouns.Country)]
-    public sealed class GetCountryCmdlet : FlightLogCmdlet<ICountry>
+    public sealed class GetCountryCmdlet : FlightLogCmdlet<ICountry>, ICountry
     {
+        [SupportsWildcards]
+        [Parameter(Mandatory = false, Position = 1)]
+        public string Name { get; set; }
+
+        int? ICountry.Id
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        string ICountry.Name
+        {
+            get
+            {
+                return (new WildcardPattern(this.Name)).ToWql();
+            }
+        }
+
+        int ICountry.CityCount
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         protected override void BeginProcessing()
         {
-            this.WriteObject(this.Repository.Retrieve(), true);
+            this.WriteObject(this.Repository.Retrieve(this), true);
         }
     }
 }
