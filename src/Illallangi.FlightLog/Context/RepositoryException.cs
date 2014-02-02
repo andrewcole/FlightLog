01d952e5e489f596ea740c136c2988dc1c2c9790
Andrew Cole <andrew.cole@illallangi.com>
2014-02-02 11:23:05 +1100

    Initial Commit of RepositoryException

---
+++ src/Illallangi.FlightLog/Context/RepositoryException.cs
@@ -0,0 +1,35 @@
+namespace Illallangi.FlightLog.Context
+{
+    using System;
+    using System.Data.SQLite;
+
+    public class RepositoryException<T> : Exception
+    {
+        private readonly T currentTarget;
+
+        private readonly string currentErrorId;
+
+        public RepositoryException(T target, SQLiteException sqe)
+            : base(sqe.Message, sqe)
+        {
+            this.currentTarget = target;
+            this.currentErrorId = sqe.ErrorCode.ToString();
+        }
+
+        public T Target
+        {
+            get
+            {
+                return this.currentTarget;
+            }
+        }
+
+        public string ErrorId
+        {
+            get
+            {
+                return this.currentErrorId;
+            }
+        }
+    }
+}
