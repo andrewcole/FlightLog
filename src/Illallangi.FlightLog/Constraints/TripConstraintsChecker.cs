83b5d3af6c0608b4f08b93c15a9e348896450816
Andrew Cole <andrew.cole@illallangi.com>
2014-02-02 11:20:31 +1100

    Added Constraints Checking for Trip

---
+++ src/Illallangi.FlightLog/Constraints/TripConstraintsChecker.cs
@@ -0,0 +1,24 @@
+namespace Illallangi.FlightLog.Constraints
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Illallangi.FlightLog.Model;
+
+    public class TripConstraintsChecker : IConstraintsChecker<ITrip>
+    {
+        public IEnumerable<string> GetErrors(ITrip newObject, params IEnumerable<ITrip>[] sets)
+        {
+            if (sets.SelectMany(s => s).Any(a => a.Year.Equals(newObject.Year) && a.Name.Equals(newObject.Name)))
+            {
+                yield return
+                    string.Format("Trip {1} ({0}) already exists", newObject.Year, newObject.Name);
+            }
+        }
+
+        public bool HasErrors(ITrip newObject, params IEnumerable<ITrip>[] sets)
+        {
+            return this.GetErrors(newObject, sets).Any();
+        }
+    }
+}
